e4eced6718b96e183cac82df4c80a96b63da0ac7
Alexander <protonull@protonmail.com>
2024-06-27 03:23:47 +0100

    Cleanup AbstractPersistentDataType

--- plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/pdc/AbstractPersistentDataType.java
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/pdc/AbstractPersistentDataType.java
@@ -2,55 +2,43 @@ package vg.civcraft.mc.civmodcore.pdc;
 
 import java.util.Objects;
 import java.util.function.Function;
-import javax.annotation.Nonnull;
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class AbstractPersistentDataType<P, C> implements PersistentDataType<P, C> {
-
     protected final Class<P> primitiveClass;
     protected final Class<C> complexClass;
 
-    public AbstractPersistentDataType(@Nonnull final Class<P> primitiveClass,
-                                      @Nonnull final Class<C> complexClass) {
+    public AbstractPersistentDataType(
+        final @NotNull Class<P> primitiveClass,
+        final @NotNull Class<C> complexClass
+    ) {
         this.primitiveClass = Objects.requireNonNull(primitiveClass);
         this.complexClass = Objects.requireNonNull(complexClass);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Nonnull
     @Override
-    public final Class<P> getPrimitiveType() {
+    public final @NotNull Class<P> getPrimitiveType() {
         return this.primitiveClass;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Nonnull
     @Override
-    public final Class<C> getComplexType() {
+    public final @NotNull Class<C> getComplexType() {
         return this.complexClass;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Nonnull
     @Override
-    public abstract P toPrimitive(@Nonnull C instance,
-                                  @Nonnull PersistentDataAdapterContext adapter);
+    public abstract @NotNull P toPrimitive(
+        @NotNull C value,
+        @NotNull PersistentDataAdapterContext adapter
+    );
 
-    /**
-     * {@inheritDoc}
-     */
-    @Nonnull
     @Override
-    public abstract C fromPrimitive(@Nonnull P raw,
-                                    @Nonnull PersistentDataAdapterContext adapter);
+    public abstract @NotNull C fromPrimitive(
+        @NotNull P raw,
+        @NotNull PersistentDataAdapterContext adapter
+    );
 
     /**
      * Convenience method to create a {@link PersistentDataType} for a flatly-valued type, like a String or an integer
