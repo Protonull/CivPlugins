4737e05df37cf2be033056bf1ae1f6b5e36f459b
Alexander <protonull@protonmail.com>
2024-06-27 03:11:39 +0100

    Add convenience method for creating flatly-valued PDTs

--- plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/pdc/AbstractPersistentDataType.java
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/pdc/AbstractPersistentDataType.java
@@ -1,9 +1,11 @@
 package vg.civcraft.mc.civmodcore.pdc;
 
 import java.util.Objects;
+import java.util.function.Function;
 import javax.annotation.Nonnull;
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class AbstractPersistentDataType<P, C> implements PersistentDataType<P, C> {
 
@@ -50,4 +52,33 @@ public abstract class AbstractPersistentDataType<P, C> implements PersistentData
     public abstract C fromPrimitive(@Nonnull P raw,
                                     @Nonnull PersistentDataAdapterContext adapter);
 
+    /**
+     * Convenience method to create a {@link PersistentDataType} for a flatly-valued type, like a String or an integer
+     * (as opposed to a record or other type of object that warrants needing the {@link PersistentDataAdapterContext}).
+     */
+    public static <P, C> @NotNull AbstractPersistentDataType<P, C> flatValued(
+        final @NotNull Class<P> primitiveClass,
+        final @NotNull Class<C> complexClass,
+        final @NotNull Function<@NotNull C, @NotNull P> encoder,
+        final @NotNull Function<@NotNull P, @NotNull C> decoder
+    ) {
+        Objects.requireNonNull(encoder);
+        Objects.requireNonNull(decoder);
+        return new AbstractPersistentDataType<>(primitiveClass, complexClass) {
+            @Override
+            public @NotNull P toPrimitive(
+                final @NotNull C value,
+                final @NotNull PersistentDataAdapterContext adapter
+            ) {
+                return encoder.apply(value);
+            }
+            @Override
+            public @NotNull C fromPrimitive(
+                final @NotNull P raw,
+                final @NotNull PersistentDataAdapterContext adapter
+            ) {
+                return decoder.apply(raw);
+            }
+        };
+    }
 }
--- plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/pdc/PersistentDataTypes.java
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/pdc/PersistentDataTypes.java
@@ -1,32 +1,19 @@
 package vg.civcraft.mc.civmodcore.pdc;
 
-import javax.annotation.Nonnull;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
-import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
 
 public final class PersistentDataTypes {
-
     public static final String DECODER_ERROR = "Was unable to decode that %s! [%s]";
 
     /**
      * Converts Components to Strings and vice versa.
      */
-    public static final PersistentDataType<String, Component> COMPONENT = new AbstractPersistentDataType<>(String.class, Component.class) {
-        @Nonnull
-        @Override
-        public String toPrimitive(@Nonnull final Component component,
-                                  @Nonnull final PersistentDataAdapterContext adapter) {
-            return GsonComponentSerializer.gson().serialize(component);
-        }
-
-        @Nonnull
-        @Override
-        public Component fromPrimitive(@Nonnull final String raw,
-                                       @Nonnull final PersistentDataAdapterContext adapter) {
-            return GsonComponentSerializer.gson().deserialize(raw);
-        }
-    };
-
+    public static final PersistentDataType<String, Component> COMPONENT = AbstractPersistentDataType.flatValued(
+        String.class,
+        Component.class,
+        GsonComponentSerializer.gson()::serialize,
+        GsonComponentSerializer.gson()::deserialize
+    );
 }
